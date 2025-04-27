edb638be3a8ff11b77c4a6b0ab7f2d52d9073a65
chetanbhati <bhatichetan147@gmail.com>
2025-04-27 18:12:21 +0530

    implemented templates in jsx

--- ecommerce-frontend/src/pages/Home.js
+++ ecommerce-frontend/src/pages/Home.js
@@ -1,29 +1,15 @@
 import React, {useEffect, useState} from "react";
-import { Link } from "react-router-dom";
-import { getapi } from "../services/Api";
+import Featured from "../components/Featured";
+import Category from "../components/Category";
+import Product from "./Product";
 
-const Home = () => {
-   const [products, setProducts] = useState([]);
-        
-          useEffect(() => {
-              getapi("store/products/")
-              .then((data) => setProducts(data))
-              .catch((err) => console.log(err));
-          }, []);
+const Home = (props) => {
 
   return (
     <div>
-      <h1>Welcome to Our E-Commerce Store</h1>
-      <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
-        {products.map((product) => (
-          <div key={product.id} style={{ border: "1px solid #ccc", padding: "15px", width: "200px" }}>
-            <img src={product.image} alt={product.name} style={{ width: "100%", height: "150px", objectFit: "cover" }} />
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
-            <Link to={`/products/${product.id}`}>View Details</Link>
-          </div>
-        ))}
-      </div>
+    <Featured />
+    <Category />
+    <Product />
     </div>
   );
 };
